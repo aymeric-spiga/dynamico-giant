efb3caffdf559e7d8f68a33d10435f33ae15084e
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2017-05-18 15:18:20 +0200

    easy install for dynamico-generic

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+rm -rf code
+svn co -q -N http://svn.lmd.jussieu.fr/Planeto/trunk code
+
+cd code
+
+echo "get DYNAMICO..."
+svn co -q http://forge.ipsl.jussieu.fr/dynamico/svn/codes/icosagcm/trunk ICOSAGCM
+
+echo "get PHYSICS..."
+svn update -q LMDZ.COMMON
+svn update -q LMDZ.GENERIC
+
+echo "get interface..."
+svn update -q ICOSA_LMDZ
+svn update -q ARCH
+
+echo "get I/O libraries..."
+svn co -q http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/trunk XIOS
+svn co -q http://forge.ipsl.jussieu.fr/heat/svn/codes/dynamico_lmdz/aquaplanet/IOIPSL
+
