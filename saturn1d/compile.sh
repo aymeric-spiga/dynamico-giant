0a8e724dbd50d0637af297035905a2b3f2030d24
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2017-07-07 15:24:07 +0200

    added folder saturn1d with compiling script

---
+++ saturn1d/compile.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+vert=64
+
+cd ../code/LMDZ.COMMON
+./makelmdz_fcm -full \
+ -b 20x25 -s 1 \
+ -t 0 \
+ -d $vert \
+ -arch X64_OCCIGEN -arch_path ../ARCH \
+ -parallel none \
+ -j 8 \
+ -p std \
+ rcm1d
+cd -
+ln -sf ../code/LMDZ.COMMON/bin/rcm1d_${vert}_phystd_seq.e rcm1d.e
+
+
