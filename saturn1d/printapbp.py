c9975988715fcff1b2aa04fb294a20f0bd85b6ea
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2017-12-17 23:05:19 +0100

    ensure the latest time is chosen in printapbp

--- saturn1d/printapbp.py
+++ saturn1d/printapbp.py
@@ -10,7 +10,7 @@ prof = open("temp_profile.txt","w")
 
 ap = pp(file=ff,x=0,y=0,var="ap").getf()
 bp = pp(file=ff,x=0,y=0,var="bp").getf()
-temp = pp(file=ff,x=0,y=0,t=1e10,var="temp").getf()
+temp = pp(file=ff,x=0,y=0,t=1e10,var="temp",useindex="1000",verbose=True).getf()
 
 for nn in range(len(ap)):
    apbp.write("%14.6e%14.6e\n"%(ap[nn],bp[nn]))
