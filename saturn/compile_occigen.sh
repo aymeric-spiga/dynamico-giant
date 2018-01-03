34db9b0c03decd570ee0c7b0ff982ef2c4344e95
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2018-01-03 11:04:52 +0100

    updated compile_occigen.sh: now svn update before compiling. better commenting.

--- saturn/compile_occigen.sh
+++ saturn/compile_occigen.sh
@@ -2,10 +2,20 @@
 
 here=$PWD
 
-cd ../code/ICOSA_LMDZ/
-./make_icosa_lmdz -p std -p_opt "-b 20x25 -s 1" -parallel mpi_omp -arch X64_OCCIGEN -arch_path ../ARCH -job 8 > $here/log_compile 2>&1
-ls -l bin/icosa_lmdz.exe
-cp -rf bin/icosa_lmdz.exe $here
-cd $here
+echo "----- update code"
+cd ../code
+svn update * | tail -n 7 > log_svn
+cat log_svn
 
-cp ../code/XIOS/bin/xios_server.exe ./
+echo "----- compile code (please wait)"
+cd ICOSA_LMDZ
+./make_icosa_lmdz -p std -p_opt "-b 20x25 -s 1" -parallel mpi_omp -arch X64_OCCIGEN -arch_path ../ARCH -job 8 > $here/log_compile 2>&1
+
+echo "----- executables should be here"
+ls -l bin/icosa_lmdz.exe
+ls -l ../XIOS/bin/xios_server.exe
+
+echo "----- copy executables to current directory"
+cd $here
+cp -rf ../code/ICOSA_LMDZ/bin/icosa_lmdz.exe ./
+cp -rf ../code/XIOS/bin/xios_server.exe ./
