fc20667476dc0bc625137c18f5da90d8f8d20a51
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2018-01-22 15:19:57 +0100

    make python plot and display directly in diag_aam.sh

--- saturn/diag_aam.sh
+++ saturn/diag_aam.sh
@@ -9,4 +9,6 @@ cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel " | sed s/'0000: AAM_vel 
 cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel+ " | sed s/'AAM_vel+ : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dissip_plus.txt
 cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel- " | sed s/'AAM_vel- : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dissip_moins.txt
 
+./diag_aam.py
 
+eog diag_aam_3_pert_log.png
