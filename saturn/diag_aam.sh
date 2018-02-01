349684a5693f23afa7d0aba39441cbe42eb49559
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2018-02-01 16:03:04 +0100

    diag_aam.sh for three digit iterations

--- saturn/diag_aam.sh
+++ saturn/diag_aam.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_dyn" | sed s/'0000: AAM_dyn budget mass + vel:'/' '/g > epsilon.txt
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_mass " | sed s/'0000: AAM_mass : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $3}' > aam_mass.txt
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $3}' > aam_vel.txt
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $4}' > aam_dissip.txt
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dyn.txt
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $6}' > aam_phys.txt
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel+ " | sed s/'AAM_vel+ : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dissip_plus.txt
-cat icosa_lmdz_?.out icosa_lmdz_??.out | grep "AAM_vel- " | sed s/'AAM_vel- : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dissip_moins.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_dyn" | sed s/'0000: AAM_dyn budget mass + vel:'/' '/g > epsilon.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_mass " | sed s/'0000: AAM_mass : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $3}' > aam_mass.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $3}' > aam_vel.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $4}' > aam_dissip.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dyn.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_vel " | sed s/'0000: AAM_vel  : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $6}' > aam_phys.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_vel+ " | sed s/'AAM_vel+ : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dissip_plus.txt
+cat icosa_lmdz_?.out icosa_lmdz_??.out icosa_lmdz_???.out | grep "AAM_vel- " | sed s/'AAM_vel- : time,old,new,dissip,dyn,phys '/' '/g | awk '{print $5}' > aam_dissip_moins.txt
 
 ./diag_aam.py
 
