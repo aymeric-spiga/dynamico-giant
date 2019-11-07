8ba8cde66459aa9d5df0f2acb113be89deedf064
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2019-11-07 11:10:42 +0100

    compile newstart and gcm at the same time

--- saturn2d/compile.sh
+++ saturn2d/compile.sh
@@ -3,22 +3,39 @@
 vert=64
 vert=32
 
+lati=128
+lati=360
+
 cd ../code/LMDZ.COMMON/ioipsl
 ./install_ioipsl_ifort.bash
 cd -
 
 cd ../code/LMDZ.COMMON
-./makelmdz_fcm -full \
+###
+./makelmdz_fcm \
  -cpp NODYN \
  -b 20x25 -s 1 \
  -t 0 \
- -d 8x128x${vert} \
+ -d 8x${lati}x${vert} \
  -arch CICLADifort \
  -parallel mpi \
  -j 8 \
  -p std \
  gcm
+###
+./makelmdz_fcm \
+ -cpp NODYN \
+ -b 20x25 -s 1 \
+ -t 0 \
+ -d 8x${lati}x${vert} \
+ -arch CICLADifort \
+ -parallel none \
+ -j 8 \
+ -p std \
+ newstart
+###
 cd -
-ln -sf ../code/LMDZ.COMMON/bin/gcm_8x128x${vert}_phystd_para.e gcm.e
+cp ../code/LMDZ.COMMON/bin/gcm_8x${lati}x${vert}_phystd_para.e ./
+cp ../code/LMDZ.COMMON/bin/newstart_8x${lati}x${vert}_phystd_seq.e ./makestart/
 
-# -t 1
+
