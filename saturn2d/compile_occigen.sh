96599eeb0f255ace8fc9167a11212c9c0cffc7c4
dbardet <dbardet@login2.occigen>
2019-11-08 15:19:31 +0100

    saturn2d: readme file and compile occigen

---
+++ saturn2d/compile_occigen.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+here=$PWD
+
+#vert=64
+#vert=32
+vert=61
+
+#lati=128
+lati=360
+
+#cd ../code/LMDZ.COMMON/ioipsl
+#./install_ioipsl_ifort.bash
+#cd -
+
+cd ../code/LMDZ.COMMON
+###
+echo "------ compile gcm (please wait) ------"
+./makelmdz_fcm \
+ -d 8x360x61 \
+ -p std \
+ -p_opt "-b 20x25 -s 1" \
+ -parallel mpi \
+ -arch X64_OCCIGEN \
+ -arch_path ../ARCH \
+ -full \
+ -job 8 \
+ -cpp NODYN \
+ gcm > $here/log_compile_gcm 2>&1
+###
+echo "------ copy gcm executable to current directory ------"
+cd $here
+cp -rf ../code/LMDZ.COMMON/bin/gcm_8x${lati}x${vert}_phystd_para.e ./
+
+cd ../code/LMDZ.COMMON
+###
+echo "------ compile newstart (please wait) ------"
+./makelmdz_fcm \
+ -d 8x360x61 \
+ -p std \
+ -p_opt "-b 20x25 -s 1" \
+ -arch X64_OCCIGEN \
+ -arch_path ../ARCH \
+ -full \
+ -job 8 \
+ -cpp NODYN \
+ newstart > $here/log_compile_newstart 2>&1
+###
+echo "------ copy newstart executable to current directory ------"
+cd $here
+cp -rf ../code/LMDZ.COMMON/bin/newstart_8x${lati}x${vert}_phystd_seq.e ./makestart/
