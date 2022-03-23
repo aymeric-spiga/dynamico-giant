36f91f5c34d63251b391cb8c5223b5fca30d3cd5
dbardet <dbardet@login1.occigen>
2022-03-23 18:02:40 +0100

    bottom of model at 20 bars

--- jupiter1d/compile.sh
+++ jupiter1d/compile.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 vert=64
-vert=32
+#vert=32
 
-cd ../code/LMDZ.COMMON
+cd ../trunk/LMDZ.COMMON
 ./makelmdz_fcm -full \
  -b 20x25 -s 2 \
  -t 0 \
@@ -14,6 +14,6 @@ cd ../code/LMDZ.COMMON
  -p std \
  rcm1d
 cd -
-ln -sf ../code/LMDZ.COMMON/bin/rcm1d_${vert}_phystd_seq.e rcm1d.e
+cp -rf ../trunk/LMDZ.COMMON/bin/rcm1d_${vert}_phystd_seq.e . 
 
 
