8036acf4ccda82306e2fac9f96e2f94058bb568d
Milcareck Gwenael <64780439+milcareck-gwenael@users.noreply.github.com>
2022-06-15 14:43:30 +0200

    Add ioipsl installation

--- install.sh
+++ install.sh
@@ -14,31 +14,36 @@ svn co -q -N http://svn.lmd.jussieu.fr/Planeto/trunk code
 
 cd code
 
-echo "1/9: get PHYSICS... (1/2) LMDZ.COMMON"
+echo "1/10: get PHYSICS... (1/2) LMDZ.COMMON"
 svn update -r $ver_phys -q LMDZ.COMMON
-echo "2/9: get PHYSICS... (2/2) LMDZ.GENERIC"
+echo "2/10: get PHYSICS... (2/2) LMDZ.GENERIC"
 svn update -r $ver_phys -q LMDZ.GENERIC
 
-echo "3/9: get interface... (1/3) ICOSA_LMDZ"
+echo "3/10: get interface... (1/3) ICOSA_LMDZ"
 svn update -r $ver_phys -q ICOSA_LMDZ
-echo "4/9: get interface... (2/3) ARCH"
+echo "4/10: get interface... (2/3) ARCH"
 svn update -r $ver_phys -q ARCH
-echo "5/9: get interface... (3/3) DOC"
+echo "5/10: get interface... (3/3) DOC"
 svn update -r $ver_phys DOC
 
-echo "6/9: get I/O libraries... (1/2) IOIPSL"
+echo "6/10: get I/O libraries... (1/2) IOIPSL"
 svn co -r $ver_xios -q http://forge.ipsl.jussieu.fr/ioserver/svn/XIOS/trunk XIOS
-echo "7/9: get I/O libraries... (2/2) XIOS"
+echo "7/10: get I/O libraries... (2/2) XIOS"
 svn co -r $ver_ioipsl -q http://forge.ipsl.jussieu.fr/heat/svn/codes/dynamico_lmdz/aquaplanet/IOIPSL
 
-echo "8/9: get FCM..."
+echo "8/10: get FCM..."
 svn -q co http://forge.ipsl.jussieu.fr/fcm/svn/PATCHED/FCM_V1.2
 echo "please add "$PWD"/FCM_V1.2/bin/ to PATH environment variable"
 
-echo "9/9: from gitlab, get DYNAMICO..."
+echo "9/10: from gitlab, get DYNAMICO..."
 git clone https://gitlab.in2p3.fr/ipsl/projets/dynamico/dynamico.git ICOSAGCM
 cd ICOSAGCM
 git checkout $ver_dyn
-cd ..
+
+echo "10/10: install ioipsl"
+cd ../LMDZ.COMMON/ioipsl/
+./install_ioipsl_occigen.bash
+cd ../../
+
 
 echo "...end"
